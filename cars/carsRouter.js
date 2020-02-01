18f86309b015cabb63d0939a13ae3cdd65cd74ad
Chris Daley <Daleys@Chriss-iMac.local>
2020-01-31 20:53:38 -0700

    Finished MVP will attempt stretch

---
+++ cars/carsRouter.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const db = require('../data/db-config');
+
+const router = express.Router();
+router.use(express.json());
+
+
+router.get('/', async ( req,res ) => {
+  try{
+    const car = await db('cars');
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({message: 'Failed to get the cars!', err});
+  };
+});
+
+router.get('/:id', async ( req,res ) => {
+  const { id } = req.params;
+  try{
+    const car = await db('cars').where('id', id);
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({message: 'Failed to get the car!', err});
+  };
+});
+
+router.post('/', async ( req,res ) => {
+  const body = req.body;
+  try{
+    const car = await db('cars').insert(body);
+    res.status(201).json(car);
+  } catch (err) {
+    res.status(500).json({message: 'Failed to update', err});
+  };
+});
+
+module.exports = router;
---
+++ migrations/20200131202315_cars.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id');
+    tbl.string('Vin Number').notNullable();
+    tbl.string('Make').notNullable();
+    tbl.string('Model').notNullable();
+    tbl.integer('Mileage').notNullable();
+    tbl.string('Transmission Type');
+    tbl.string('Title Status');
+})
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
