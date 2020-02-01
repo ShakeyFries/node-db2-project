946e57abd544a1db3043de95cb09dad1a6afcc45
Chris Daley <Daleys@Chriss-iMac.local>
2020-01-31 21:01:29 -0700

    added a put and delete end ppoints, tested in postman and they are functional. some stretch is done

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -35,4 +35,29 @@ router.post('/', async ( req,res ) => {
   };
 });
 
+router.put('/:id', async ( req,res ) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  try{
+    const car = await db('cars').where('id', id)
+    .update(body);
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({message: 'Failed to get the car!', err});
+  };
+});
+
+router.delete('/:id', async ( req,res ) => {
+  const { id } = req.params;
+
+  try{
+    const car = await db('cars').where('id', id)
+    .del();
+    res.status(200).json(car);
+  } catch (err) {
+    res.status(500).json({message: 'Failed to get the car!', err});
+  };
+});
+
 module.exports = router;
